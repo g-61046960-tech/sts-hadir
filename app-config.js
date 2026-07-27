f9597fcab1b77bbe2b9141af7f243aa6cceee512
g-61046960-tech <g-61046960@moe-dl.edu.my>
2026-07-27 19:18:58 +0800

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,16 @@
+window.APP_CONFIG = {
+  // Gantikan dengan URL Web App Google Apps Script yang berakhir dengan /exec
+  webAppUrl: "https://script.google.com/macros/s/AKfycbz7tFx6LfKuZhelpoep-mw_SZyehwEDYHBoTgHF9mKsz2kxqeuc9XIAJ9-NVH4xN1_v/exec",
+  admin: { username: "jeb7029", password: "admin" },
+  teachers: {
+    "Perdana": [
+      "Contoh Guru Perdana 1",
+      "Contoh Guru Perdana 2",
+      "Contoh Guru Perdana 3"
+    ],
+    "Tingkatan 6": [
+      "Contoh Guru Tingkatan 6 1",
+      "Contoh Guru Tingkatan 6 2"
+    ]
+  }
+};
